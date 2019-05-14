ef927a2c2022767e6a2af3c3a7794b75e74b1552
Sameer Totey <sameertotey@hotmail.com>
2019-05-13 21:11:24 -0600

    another commit

--- azuremobile_sbt/AppDelegate.cs
+++ azuremobile_sbt/AppDelegate.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using Foundation;
 using UIKit;
 
@@ -14,5 +15,48 @@ namespace azuremobile_sbt
 	{
 		// class-level declarations
 		public override UIWindow Window {get; set;}
-	}
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            //AppCenter.Start(
+            //    IOSConfig.AppCenterSecret,
+            //    typeof(Analytics),
+            //    typeof(Crashes),
+            //    typeof(Distribute)
+            //);
+
+            //var version = $"Version: {NSBundle.MainBundle.InfoDictionary["CFBundleShortVersionString"]} - {NSBundle.MainBundle.InfoDictionary["CFBundleVersion"]}";
+            //CrossSettings.Current.AddOrUpdateValue(Config.Version, version);
+
+            //var host = CrossSettings.Current.GetValueOrDefault(Config.Host, -1);
+
+            //if ((host == -1) || (Host)host == Host.Prod)
+            //{
+            //    RestEndpoints.MobileAppUrl = RestEndpoints.ProdMobileAppUrl;
+            //    RestEndpoints.ResourceID = RestEndpoints.ProdResourceID;
+            //    RestEndpoints.ServiceBusConnectionString = RestEndpoints.ProdServiceBusConnectionString;
+            //    CrossSettings.Current.AddOrUpdateValue(Config.Host, (int)Host.Prod);
+            //}
+            //else
+            //{
+            //    RestEndpoints.MobileAppUrl = RestEndpoints.QAMobileAppUrl;
+            //    RestEndpoints.ResourceID = RestEndpoints.QAResourceID;
+            //    RestEndpoints.ServiceBusConnectionString = RestEndpoints.QAServiceBusConnectionString;
+            //}
+
+
+            //PinnacleApp.Initialize(
+            //    new Lazy<StandardKernel>(() => new StandardKernel(new IosIocModule()))
+            //);
+
+            ServicePointManager.ServerCertificateValidationCallback +=
+                (sender, cert, chain, sslPolicyErrors) => true;
+
+            Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
+
+            //(application as PinnacleApplication).SessionTimeoutService = PinnacleApp.Get<ISessionTimeoutService>();
+
+            return true;
+        }
+    }
 }
